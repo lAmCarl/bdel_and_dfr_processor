c27fd45da17ef8034c5b7a8c259fa087f0162525
Raekye <adrian@creatifcubed.com>
2014-11-13 23:59:45 -0500

    update assembler

--- assembler.pl
+++ assembler.pl
@@ -55,46 +55,52 @@ sub process_instruction {
 	my @match;
 	if ($instr =~ /eof/) {
 		print_binary_instruction(0);
-	} elsif (@match = $instr =~ /(load) (\d+) (\d+)/) {
+	} elsif (@match = $instr =~ /(load) (\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($match[0]), $match[1], $match[2]);
-	} elsif ($instr =~ /(store) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(store) r(\d+) (\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3);
-	} elsif ($instr =~ /(literal) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(literal) (\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3);
-	} elsif ($instr =~ /(input) (\d+)/) {
+	} elsif ($instr =~ /(input) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
-	} elsif ($instr =~ /(output) (\d+)/) {
+	} elsif ($instr =~ /(output) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
-	} elsif ($instr =~ /(add) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(add) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(sub) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(sub) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(mul) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(mul) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(div) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(div) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(cmp) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(cmp) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(branch) (\d+)/) {
+	} elsif ($instr =~ /(branch) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
-	} elsif ($instr =~ /(jump) ([a-zA-Z]\w*)/) {
-		if (exists($labels->{$2})) {
-			print_binary_instruction(binary_opcode_from_string($1), $labels->{$2});
+	} elsif ($instr =~ /(jump) (\d) ([a-zA-Z]\w*)/) {
+		if (exists($labels->{$3})) {
+			print_binary_instruction(binary_opcode_from_string($1), $2, $labels->{$3});
 		} else {
-			die "Unknown label \"$2\"";
+			die "Unknown label \"$3\"";
 		}
+	} elsif ($instr =~ /(jump) (\d) r(\d+)/) {
+		print_binary_instruction(binary_opcode_from_string($1, $2, $3));
 	} elsif ($instr =~ /(stack) (\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
 	} elsif ($instr =~ /(supermandive)/) {
 		print_binary_instruction(binary_opcode_from_string($1));
 	} elsif ($instr =~ /(getup)/) {
 		print_binary_instruction(binary_opcode_from_string($1));
-	} elsif ($instr =~ /(print) (\d+)/) {
+	} elsif ($instr =~ /(print) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
-	} elsif ($instr =~ /(draw) (\d+) (\d+) (\d+)/) {
+	} elsif ($instr =~ /(draw) r(\d+) r(\d+) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2, $3, $4);
-	} elsif ($instr =~ /(keyboard) (\d+)/) {
+	} elsif ($instr =~ /(keyboard) r(\d+)/) {
 		print_binary_instruction(binary_opcode_from_string($1), $2);
+	} elsif ($instr =~ /(heap) r(\d+) r(\d+)/) {
+		print_binary_instruction(binary_opcode_from_string($1, $2, $3));
+	} elsif ($instr =~ /(unheap) r(\d+) r(\d+)/) {
+		print_binary_instruction(binary_opcode_from_string($1, $2, $3));
 	} else {
 		die "Unknown instruction \"$instr\"";
 	}
@@ -122,6 +128,8 @@ sub binary_opcode_from_string {
 		'print' => 16,
 		'draw' => 17,
 		'keyboard' => 18,
+		'heap' => 19,
+		'unheap' => 20,
 	);
 	return $map{$_[0]};
 }
